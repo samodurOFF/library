b9771b374b051281ba9d89998a807da1a5f42789
Mutaf-Volodymyr <mutaf.dev@gmail.com>
2025-02-14 09:17:57 +0100

    format code black

--- library/models/book.py
+++ library/models/book.py
@@ -4,20 +4,24 @@ from django.db import models
 
 class Book(models.Model):
     CHOICES = (
-        ('fiction', 'Fiction'),
-        ('non-fiction', 'Non-Fiction'),
-        ('science fiction', 'Science Fiction'),
-        ('fantasy', 'Fantasy'),
-        ('mystery', 'Mystery'),
-        ('biography', 'Biography'),
-
+        ("fiction", "Fiction"),
+        ("non-fiction", "Non-Fiction"),
+        ("science fiction", "Science Fiction"),
+        ("fantasy", "Fantasy"),
+        ("mystery", "Mystery"),
+        ("biography", "Biography"),
     )
-    title = models.CharField(max_length=255, verbose_name='Название', )
-    author_id = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    title = models.CharField(
+        max_length=255,
+        verbose_name="Название",
+    )
+    author_id = models.ForeignKey("Author", on_delete=models.SET_NULL, null=True)
     publish_date = models.DateField()
     description = models.TextField(null=True, blank=True)
     genre = models.CharField(max_length=50, choices=CHOICES, null=True, blank=True)
-    pages = models.IntegerField(null=True, blank=True, validators=[MaxValueValidator(1000)])
+    pages = models.IntegerField(
+        null=True, blank=True, validators=[MaxValueValidator(1000)]
+    )
 
     def __str__(self):
         return self.title
