0c203f5c1c5e583a500cb22995c9501beeda4f61
Danil <samodurOFF@users.noreply.github.com>
2025-03-02 14:33:16 +0500

    Views was change through GenericAPIView

--- library/views.py
+++ library/views.py
@@ -1,29 +1,29 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
-
 from library.models import Book
 from library.serializers import BookListSerializer, BookDetailSerializer, BookCreateSerializer
 from rest_framework.pagination import PageNumberPagination
 
 
-class BookListView(APIView, PageNumberPagination):
-    page_size = 5
+class BookListView(GenericAPIView):
+    queryset = Book.objects.all()
+    pagination_class = PageNumberPagination
+    pagination_class.page_size = 5
+    page_size = pagination_class.page_size
 
-    def get(self, request):
+    def get(self, request, *args, **kwargs):
         year = request.query_params.get('year')
         page_size = self.get_page_size(request)
 
         if year:
             books = Book.objects.filter(publish_date__year=year)
-            serializer = BookListSerializer(books, many=True)
+            serializer = self.get_serializer(books, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
 
-        books = Book.objects.all()
         self.page_size = page_size  # Установка значения page_size
-        results = self.paginate_queryset(books, request, view=self)
-        serializer = BookDetailSerializer(results, many=True)
+        results = self.paginate_queryset(self.get_queryset())
+        serializer = self.get_serializer(results, many=True)
         return self.get_paginated_response(serializer.data)
 
     def get_page_size(self, request):
@@ -32,59 +32,53 @@ class BookListView(APIView, PageNumberPagination):
             return int(page_size)
         return self.page_size  # Использование значения по умолчанию
 
-    def post(self, request):
-        serializer = BookCreateSerializer(data=request.data)
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def get_serializer_class(self):
 
-class BookDetailView(APIView):
+        if self.request.method == 'POST':
+            return BookCreateSerializer  # Использование другого сериализатора для POST-запросов
+        elif self.request.method == 'GET':
+            return BookListSerializer
 
-    def get_book(self, pk):
-        try:
-            return Book.objects.get(pk=pk)
-        except Book.DoesNotExist:
-            return None
 
-    def get(self, request, pk):
-        book = self.get_book(pk)
-        if book is None:
-            return Response({'error': 'Book not found'}, status=status.HTTP_404_NOT_FOUND)
+class BookDetailView(GenericAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookDetailSerializer
 
-        serializer = BookDetailSerializer(book)
+    def get(self, request, *args, **kwargs):
+        book = self.get_object()
+
+        serializer = self.get_serializer(book)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def put(self, request, pk):
-        book = self.get_book(pk)
-        if book is None:
-            return Response({'error': 'Book not found'}, status=status.HTTP_404_NOT_FOUND)
+    def put(self, request, *args, **kwargs):
+        book = self.get_object()
 
-        serializer = BookDetailSerializer(book, data=request.data)
+        serializer = self.get_serializer(book, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def patch(self, request, pk):
-
-        book = self.get_book(pk)
-        if book is None:
-            return Response({'error': 'Book not found'}, status=status.HTTP_404_NOT_FOUND)
-
-        serializer = BookDetailSerializer(book, data=request.data, partial=True)
+    def patch(self, request, *args, **kwargs):
+        book = self.get_object()
+        serializer = self.get_serializer(book, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
+            print(serializer.data, serializer.validated_data)
             return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk):
-        book = self.get_book(pk)
-        if book is None:
-            return Response({'error': 'Book not found'}, status=status.HTTP_404_NOT_FOUND)
+    def delete(self, request, *args, **kwargs):
+        book = self.get_object()
 
         book.delete()
         return Response({'message': 'Book was deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
