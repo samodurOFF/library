32dd7fc7edabb6e6d599299707d843bad9652700
AndreyLapko1 <andreycgo765@gmail.com>
2025-03-25 14:47:00 +0300

    add JWT token auth

--- library/urls.py
+++ library/urls.py
@@ -3,11 +3,14 @@ from rest_framework.routers import DefaultRouter
 from . import views
 from .views import CategoryDetailUpdateDeleteView
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 router = DefaultRouter()
 router.register(r'categories', CategoryDetailUpdateDeleteView)
 urlpatterns = [
     path('book_list/', views.BookListView.as_view(), name='book_list'),
     path('book_list/<int:pk>', views.BookDetailView.as_view(), name='book_detail'),
     path('', include(router.urls), name='category_detail'),
-    path('oauth/', obtain_auth_token, name='auth_token')
+    path('oauth/', obtain_auth_token, name='auth_token'),
+    path('token-refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token-obtain/', TokenObtainPairView.as_view(), name='token_obtain'),
 ]
--- library_project/settings.py
+++ library_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+from datetime import timedelta
 from pathlib import Path
 from environ import Env
 
@@ -142,11 +143,17 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         # 'rest_framework.authentication.BasicAuthentication',
         # 'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
 LOGGING = {
