c76bb83a283c3be266edcd8410d4626ff704b709
Mutaf-Volodymyr <mutaf.dev@gmail.com>
2025-03-28 12:04:12 +0200

    registrate middleware

--- library_project/settings.py
+++ library_project/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'library.middleware.JWTAuthenticationMiddleware'
 ]
 
 ROOT_URLCONF = "library_project.urls"
